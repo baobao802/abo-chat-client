04201732ba80e932c888e6e3edc014c07f26b092
bao_bao <bluedev82@gmail.com>
2021-11-20 12:23:33 +0700

    update env

--- src/app/core/ws/index.js
+++ src/app/core/ws/index.js
@@ -5,7 +5,8 @@ import localStore from '../../shared/utils/localStore';
 const ws = Stomp.over(
   () =>
     new SockJS(
-      process.env.REACT_APP_BASE_URL || 'http://localhost:8080/api/v1/'
+      `${process.env.REACT_APP_BASE_URL}/ws` ||
+        'http://localhost:8080/api/v1/ws'
     )
 );
 
